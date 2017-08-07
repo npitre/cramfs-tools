404435a6f3d23bf50e5f09c030c59e7ba2b818ad
Nicolas Pitre <nico@fluxnic.net>
2017-08-07 00:54:32 -0400

    improve compression statistics reporting
    Especially now that compression can be skipped when not beneficial,
    knowing the actual overhead becomes interesting.
    
    Some code/variable cleanups.

--- mkcramfs.c
+++ mkcramfs.c
@@ -709,13 +709,9 @@ static int is_zero(unsigned char const *begin, unsigned len)
 static unsigned int do_compress(unsigned char *base, unsigned int offset, struct entry *entry)
 {
 	unsigned int size = entry->size;
-	unsigned long original_size = size;
-	unsigned long original_offset = offset;
-	unsigned long new_size;
-	unsigned long blocks = (size - 1) / blksize + 1;
-	unsigned long curr = offset + 4 * blocks;
-	int change;
-	unsigned char *uncompressed = entry->uncompressed;
+	unsigned int blocks = (size - 1) / blksize + 1;
+	unsigned int curr = offset + 4 * blocks;
+	unsigned char *data = entry->uncompressed;
 
 	total_blocks += blocks; 
 
@@ -726,40 +722,44 @@ static unsigned int do_compress(unsigned char *base, unsigned int offset, struct
 		if (input > blksize)
 			input = blksize;
 		size -= input;
-		if (is_zero(uncompressed, input)) {
+		if (is_zero(data, input)) {
 			super_flags |= CRAMFS_FLAG_HOLES;
 		} else {
-			compress(base + curr, &len, uncompressed, input);
+			compress(base + curr, &len, data, input);
 			if (len > blksize*2) {
 				/* (I don't think this can happen with zlib.) */
 				error_msg_and_die("AIEEE: block \"compressed\" to > 2*blocklength (%ld)\n", len);
 			}
 			if (opt_extblkptr && len >= input) {
 				/* Better keep it uncompressed */
+				if (opt_verbose > 2)
+					printf("literal %d vs compressed %ld\n",
+						input, len);
 				len = input;
-				memcpy(base + curr, uncompressed, len);
+				memcpy(base + curr, data, len);
 				flags = CRAMFS_BLK_FLAG_UNCOMPRESSED;
 				super_flags |= CRAMFS_FLAG_EXT_BLOCK_POINTERS;
 			}
 			curr += len;
 		}
-		uncompressed += input;
+		data += input;
 
 		*(u32 *) (base + offset) = curr | flags;
 		offset += 4;
 	} while (size);
 
-	curr = (curr + 3) & ~3;
-	new_size = curr - original_offset;
-	/* TODO: Arguably, original_size in these 2 lines should be
-	   st_blocks * 512.  But if you say that then perhaps
-	   administrative data should also be included in both. */
-	change = new_size - original_size;
 	if (opt_verbose > 1) {
-	    printf("%6.2f%% (%+d bytes)\t%s\n",
-		    (change * 100) / (double) original_size, change, entry->name);
+		/* offset now points at the first block */
+		int new_size = curr - offset;
+		int change = new_size - entry->size;
+		int last_alignment = (4 - curr) & 3;
+		int overhead = blocks * 4 + last_alignment;
+		printf("%6.2f%% (%+d bytes, %d overhead)\t%s\n",
+		       (change + overhead)*100.0 / entry->size,
+		       change, overhead, entry->name);
 	}
 
+	curr = (curr + 3) & ~3;
 	return curr;
 }
 
